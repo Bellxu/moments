f7b36349d67389684cfe8919905f010bc240deee
bellxu <345976354@qq.com>
2020-07-08 21:53:32 +0800

    修改登陆界面

--- app/src/main/java/com/dajj/moment/activity/LoginActivity.java
+++ app/src/main/java/com/dajj/moment/activity/LoginActivity.java
@@ -3,6 +3,8 @@ package com.dajj.moment.activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
@@ -11,8 +13,12 @@ import com.dajj.moment.R;
 
 public class LoginActivity extends BaseActvity implements View.OnClickListener {
 
-    private Button logon_button;
-    private Button login_button;
+    private Button login;
+    private TextView password_login;
+    private TextView logon;
+    private EditText edit1;
+    private EditText edit2;
+    private boolean isPasswordLogin=false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,19 +28,51 @@ public class LoginActivity extends BaseActvity implements View.OnClickListener {
     }
 
     private void initView() {
-        login_button = findViewById(R.id.login);
-        logon_button = findViewById(R.id.logon);
-        login_button.setOnClickListener(this);
-        logon_button.setOnClickListener(this);
+        login = findViewById(R.id.login);
+        password_login = findViewById(R.id.password_login);
+        logon = findViewById(R.id.logon);
+        edit1 = findViewById(R.id.edit1);
+        edit2 = findViewById(R.id.edit2);
+        login.setOnClickListener(this);
+        password_login.setOnClickListener(this);
+        logon.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.login:
+                doLogin();
                 break;
             case R.id.logon:
+                doLogon();
                 break;
+            case R.id.password_login:
+                ChangeToPasswordLogin();
+                break;
+        }
+
+    }
+
+    private void ChangeToPasswordLogin() {
+        isPasswordLogin=true;
+
+    }
+
+    private void doPasswordLogin() {
+    }
+
+    private void doLogon() {
+        goToLogOn();
+    }
+
+    private void goToLogOn() {
+
+    }
+
+    private void doLogin() {
+        if (!isPasswordLogin) {
+            
         }
 
     }
